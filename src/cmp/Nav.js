8b7a004195dbf2bde1bb80b8a16f70ec3a16026b
Oliver Bath <oliverbth05@gmail.com>
2020-05-29 15:49:52 -0400

    Pre deploy

--- src/cmp/Nav.js
+++ src/cmp/Nav.js
@@ -6,15 +6,15 @@ import { NavLink } from 'react-router-dom';
 class Nav extends React.Component {
     render() {
         return (
-            <nav className = 'nav bg-white b-b-1 b-grey-2'>
-                <div className = 'nav__container'>
-                    <h3 className = 'color-primary font-light'>Palettes</h3>
-                    <div className = 'nav__links'>
-                        <NavLink to = '/create-palette' activeClassName = 'nav__link-active' className = 'nav__link'>New Palette</NavLink>
-                        <NavLink to = '/my-palettes' activeClassName = 'nav__link-active' className = 'nav__link'>My Palettes</NavLink>
+            <nav className='nav bg-white b-b-1 b-grey-2'>
+                <div className='nav__container'>
+                    <h3 className='color-primary font-light'>Color Palette Maker</h3>
+                    <div className='nav__links'>
+                        <NavLink to='/create-palette' activeClassName='nav__link-active' className='nav__link'>New Palette</NavLink>
+                        <NavLink to='/my-palettes' activeClassName='nav__link-active' className='nav__link'>My Palettes</NavLink>
                     </div>
                 </div>
-            </nav>    
+            </nav>
         )
     }
 }
--- src/views/MyPalettes.js
+++ src/views/MyPalettes.js
@@ -11,26 +11,29 @@ class MyPalettes extends React.Component {
 
     renderPalettes() {
         return this.props.myPalettes.map(palette => {
-            return <PaletteCard key = {palette.id} {...palette}/>
+            return <PaletteCard key={palette.id} {...palette} />
         })
     }
- 
-    render() { 
-        
+
+    render() {
+
         if (this.props.myPalettes.length < 1) {
             return (
-                <div className = 'container container-full-section container-flex-center'>
-                    <h2>You haven't saved any palettes</h2>
-                    <Link className = 'btn btn-primary btn-block text-center p-a-2' to = 'create-palette'>Create One</Link>
+                <div className='container container-full-section container-flex-center'>
+                    <div>
+                        <h3>You haven't saved any palettes</h3>
+                        <Link className='btn btn-primary btn-block btn-round text-center p-a-2' to='create-palette'>Create One</Link>
+                    </div>
+
                 </div>
             )
         }
-        
+
         return (
-            <div className = 'container-res-1000 p-a-3'>
-              <div className=  'palette-grid'>
-                {this.renderPalettes()}
-              </div>
+            <div className='container-res-1000 p-a-3'>
+                <div className='palette-grid'>
+                    {this.renderPalettes()}
+                </div>
             </div>
         )
     }
